0ff02f1372dada7f6642dba0c0d82a6d10e06e4e
2014105097 <2014105097@B28.com.knu.ac.kr>
2016-11-18 20:07:38 +0900

    servermainpanel

--- ConnectedCarService/src/server/gui/ServerMainPanel.java
+++ ConnectedCarService/src/server/gui/ServerMainPanel.java
@@ -6,12 +6,25 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import common.CarAttribute;
+import common.MapDataFetcher;
+import common.Point;
+import common.gui.ImagePanel;
 import common.gui.MyPanel;
 import server.MyCar;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ServerMainPanel extends JPanel implements Runnable {
 	
 	private MyCar car; 
+	private ImagePanel mapPanel;
+	private JButton btnSetDest;
+	private JButton btnFind;
+	private JLabel lblConnectedCar;
 	
 	public ServerMainPanel(CarAttribute attr, boolean isDebug) throws IOException{
 		
@@ -19,21 +32,56 @@ public class ServerMainPanel extends JPanel implements Runnable {
 		
 		setSize(1008, 638);
 		
-		JButton btnNewButton_1 = new JButton("SERVER");
-		btnNewButton_1.setBounds(706, 389, 139, 23);
-		add(btnNewButton_1);
+		btnSetDest = new JButton("Set Destination");
+		btnSetDest.setActionCommand("1");
+		btnSetDest.setBounds(81, 564, 220, 57);
+		add(btnSetDest);
+		
+		btnFind = new JButton("");
+		btnFind.setActionCommand("2");
+		btnFind.setBounds(358, 564, 240, 57);
+		add(btnFind);
+		
+		lblConnectedCar = new JLabel("Connected Car : None");
+		lblConnectedCar.setFont(new Font("±¼¸²", Font.PLAIN, 20));
+		lblConnectedCar.setBounds(664, 578, 264, 24);
+		add(lblConnectedCar);
 		
 		setVisible(true);
 		
 		car = new MyCar(attr);
 		if (isDebug) {
-			
+			car.setCurPos(new Point(35.892441,128.609169));
 			
 		} else {
 			new Thread(car).start();
 		}
 		
+		mapPanel = new ImagePanel(MapDataFetcher.getCurImage(car.getCurPos(), car.getAttr().getNum()));
+		mapPanel.setBounds(0, 0, 1008, 550);
+		
+		add(mapPanel);
+		
 		//new Thread(this).run();
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		JButton btn = (JButton) arg0.getSource();
+		char command = btn.getActionCommand().charAt(0);
+		
+		switch(command) {
+		case 1:  // Set Destination
+			
+			break;
+		case 2:  // Find Car
+			if (btn.isSelected()) {  // ON
+				
+			} else {  // OFF
+				// Close safely
+			}
+			break;
+		}
+		
 	}
 
 	@Override
